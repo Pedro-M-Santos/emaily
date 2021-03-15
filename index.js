457c4ea7cfa086a038b522bbc9b1b2f7cd8277cf
MatiasPedro25 <unbmatiaspedro@gmail.com>
2021-03-15 09:06:56 -0300

    Modulo 4

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+require('./models/Users')
 require('./services/passport');
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 
 const app = express();
+
+// Google Oauth routes
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000; 
+// Connecting to MongoDB
+const connectMongoDB = async () =>{
+    console.log('Trying to connect to Database')
+    try{
+        await mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+connectMongoDB()
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,31 @@
-const passport = require('passport');
+const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret, 
-    callbackURL: '/auth/google/callback'
-},(accessToken,refreshToken,profile,done) =>console.log(accessToken,refreshToken,profile) ));
+const User = mongoose.model('users')
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({googleId:profile.id})
+      .then((existingUser)=>{
+        if(existingUser){
+          console.log("This user is already registered")
+          done(null,existingUser)
+        }
+        else{
+          console.log("New user!")
+          new User({googleId: profile.id})
+          .save()
+          .then(user=> done(null,user))
+        }
+      })
+    }
+  )
+);
